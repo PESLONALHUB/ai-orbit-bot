cb5944724cdd8cd5928eff7f571a68ff7ac19613
PESLONALHUB <rozlove687@gmail.com>
2026-07-12 10:36:26 +0530

    Add config.py
    Initialize configuration settings for the Telegram bot, including tokens, file paths, and default hashtags.

---
+++ config.py
@@ -0,0 +1,28 @@
+import os
+
+# Telegram
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME")
+
+# Files
+FEEDS_FILE = "feeds.txt"
+
+DATA_FOLDER = "data"
+POSTED_FILE = f"{DATA_FOLDER}/posted.json"
+LOG_FILE = f"{DATA_FOLDER}/logs.txt"
+
+# Telegram
+PARSE_MODE = "HTML"
+DISABLE_WEB_PREVIEW = False
+
+# Bot
+MAX_POSTS_PER_RUN = 1
+REQUEST_TIMEOUT = 20
+
+DEFAULT_HASHTAGS = [
+    "#AI",
+    "#ArtificialIntelligence",
+    "#ChatGPT",
+    "#Tech",
+    "#AITools"
+]
